36ee65e56aadfb97c0765f48dac24cabfe7b6109
rakhmatovsanjar <sanjarbekrahmatov2003@gmail.com>
2024-03-29 17:25:56 +0500

    the part up to the calendar page is ready

--- app/src/main/java/com/example/mobileuikit/CalendarPageActivity.kt
+++ app/src/main/java/com/example/mobileuikit/CalendarPageActivity.kt
@@ -1,23 +1,15 @@
 package com.example.mobileuikit
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class BlogPostPageActivity : AppCompatActivity() {
+class CalendarPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_blog_post_page)
-
-        findViewById<CardView>(R.id.post_1).setOnClickListener {
-            startActivity(
-                Intent(this,ExpensesPageActivity::class.java)
-            )
-        }
+        setContentView(R.layout.activity_calendar_page)
     }
 }
--- app/src/main/java/com/example/mobileuikit/ExpensesPageActivity.kt
+++ app/src/main/java/com/example/mobileuikit/ExpensesPageActivity.kt
@@ -2,21 +2,21 @@ package com.example.mobileuikit
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class BlogPostPageActivity : AppCompatActivity() {
+class ExpensesPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_blog_post_page)
+        setContentView(R.layout.activity_expenses_page)
 
-        findViewById<CardView>(R.id.post_1).setOnClickListener {
+        findViewById<TextView>(R.id.expenses_page_item_1).setOnClickListener {
             startActivity(
-                Intent(this,ExpensesPageActivity::class.java)
+                Intent(this,CalendarPageActivity::class.java)
             )
         }
     }
